d13a1bee28539b012e3037d6521626a3ce569087
IliesBourib <bourib@mediamarktsaturn.com>
2019-10-08 23:33:43 +0200

    tut react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class HelloReact extends React.Component {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    constructor (props) {
+        super(props);
+
+        this.state =  {
+            anzahl: 1
+        };
+    }
+    static defaultProps = {name: "Christian"}
+
+    componentDidMount (){
+        this.setState({anzahl: this.state.anzahl +5});
+    };
+
+  onButtonClick(){
+    this.setState({anzahl: this.state.anzahl +1});  }
+
+    render() {
+        return (
+            <div>
+        <h1>
+            <Greeting welcome={this.props.welcome} /> {this.props.name}
+            </h1>
+            <p>{this.state.anzahl}</p>
+            <button onClick={this.onButtonClick.bind(this)}>Klick mich!</button>
+            </div>
+        );
+    }
+}
+
+class Greeting extends React.Component {
+    render (){
+        if(this.props.welcome === true)
+        return <span>Willkommen</span>;
+        else
+         return <span>Tschüüüüüs</span>;
+    }
+}
+
+ReactDOM.render(<HelloReact welcome={true}/>, document.getElementById('root'));
+
